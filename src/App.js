efc6ab6841378f4fcc1dcdc6cdfe34eb7039d1af
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2023-04-03 16:31:00 +0600

    admin pages impleminted

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
-import StudenntRegister from './Pages/Students/Authentication/StudenntRegister';
-import StudentLogin from './Pages/Students/Authentication/StudentLogin';
-import CoursePlayer from './Pages/Students/CoursePlayer/CoursePlayer';
-import LeaderBoard from './Pages/Students/LeaderBoard/LeaderBoard';
-import Quiz from './Pages/Students/Quiz/Quiz';
+import AssignmentMarks from './Pages/Admin/AssignmentMarks/AssignmentMarks';
+import Assignments from './Pages/Admin/Assignments/Assignments';
+import AdminLogin from './Pages/Admin/Authentication/AdminLogin';
+import Dashboard from './Pages/Admin/Dashboard/Dashboard';
+import Quizes from './Pages/Admin/Quizes/Quizes';
+import Videos from './Pages/Admin/Videos/Videos';
+// import StudenntRegister from './Pages/Students/Authentication/StudenntRegister';
+// import StudentLogin from './Pages/Students/Authentication/StudentLogin';
+// import CoursePlayer from './Pages/Students/CoursePlayer/CoursePlayer';
+// import LeaderBoard from './Pages/Students/LeaderBoard/LeaderBoard';
+// import Quiz from './Pages/Students/Quiz/Quiz';
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
-    <CoursePlayer></CoursePlayer>
+    {/* <CoursePlayer></CoursePlayer>
     <LeaderBoard></LeaderBoard>
     <Quiz></Quiz>
     <StudentLogin></StudentLogin>
-    <StudenntRegister></StudenntRegister>
+    <StudenntRegister></StudenntRegister> */}
+    <AdminLogin></AdminLogin>
+    <Assignments></Assignments>
+    <AssignmentMarks></AssignmentMarks>
+    <Dashboard></Dashboard>
+    <Quizes></Quizes>
+    <Videos></Videos>
     </>
   );
 }
