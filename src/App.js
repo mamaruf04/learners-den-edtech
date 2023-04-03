e1ae7d50171ccae7337303322bddaa26137a4a5e
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2023-04-03 16:15:23 +0600

    students pages implimented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Component/Navbar/Navbar';
+import StudenntRegister from './Pages/Students/Authentication/StudenntRegister';
+import StudentLogin from './Pages/Students/Authentication/StudentLogin';
+import CoursePlayer from './Pages/Students/CoursePlayer/CoursePlayer';
+import LeaderBoard from './Pages/Students/LeaderBoard/LeaderBoard';
+import Quiz from './Pages/Students/Quiz/Quiz';
 
 function App() {
   return (
     <>
-    
+    <Navbar></Navbar>
+    <CoursePlayer></CoursePlayer>
+    <LeaderBoard></LeaderBoard>
+    <Quiz></Quiz>
+    <StudentLogin></StudentLogin>
+    <StudenntRegister></StudenntRegister>
     </>
   );
 }
